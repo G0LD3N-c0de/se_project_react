011dbbf87dc3e16043b9a05e999675cf38000035
SpaceMan28 <nicokazemi34@gmail.com>
2023-07-19 10:09:56 -0600

    style: style item card popup and add like button to clothing items

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -3,7 +3,13 @@ import "../blocks/ItemCard.css";
 function ItemCard({ i, onSelectCard }) {
   return (
     <div className="card">
-      <div className="card__title">{i.name}</div>
+      <div className="card__header">
+        <div className="card__title">{i.name}</div>
+        <img
+          className="card__heart"
+          src={require("../images/heart.svg").default}
+        />
+      </div>
       <img
         src={i.link}
         alt="clothing item"
--- src/components/ItemModal.js
+++ src/components/ItemModal.js
@@ -1,11 +1,17 @@
+import "../blocks/ItemModal.css";
+
 function ItemModal({ selectedCard, onClose }) {
   return (
     <div className="modal">
-      <div className="modal__content">
-        <button type="button" onClick={onClose} className="modal__close" />
-        <img src={selectedCard.link} />
-        <div>{selectedCard.name}</div>
-        <div>{selectedCard.weather}</div>
+      <div className="modal__content modal__preview-content">
+        <button
+          type="button"
+          onClick={onClose}
+          className="modal__close modal__item-close"
+        />
+        <img className="item-modal__image" src={selectedCard.link} />
+        <div className="item-modal__title">{selectedCard.name}</div>
+        <div className="item-modal__weather">{selectedCard.weather}</div>
       </div>
     </div>
   );
