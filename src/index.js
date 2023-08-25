617604d3b572cc2058cc68d909d0e23dd61be624
SpaceMan28 <nicokazemi34@gmail.com>
2023-08-25 16:18:25 -0600

    add variable

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  BrowserRouter,
+  HashRouter,
+} from "react-router-dom/cjs/react-router-dom.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
