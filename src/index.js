c2fd1c20806811dd4d3726c72ec3913dae1311c0
SpaceMan28 <nicokazemi34@gmail.com>
2023-08-25 16:42:24 -0600

    add profile to db.json

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
-import {
-  BrowserRouter,
-  HashRouter,
-} from "react-router-dom/cjs/react-router-dom.min";
+import { HashRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
