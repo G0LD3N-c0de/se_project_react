82800e53619e69eca6dbcccf1e933776f30e14d1
SpaceMan28 <nicokazemi34@gmail.com>
2023-08-25 16:17:19 -0600

    add hasrouter

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 reportWebVitals();
